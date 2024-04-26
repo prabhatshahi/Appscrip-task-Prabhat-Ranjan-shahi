1ad9d73f59ac9e356bef4ef6dd6af66f3c3889f2
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-04-26 19:11:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import AddToCart from "./components/cart/AddToCart";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header /> {/* Render the Header component */}
+        <Routes>
+          {" "}
+          <Route exact path="/" element={<Body />} />{" "}
+          <Route path="/add-to-cart" element={<AddToCart />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
